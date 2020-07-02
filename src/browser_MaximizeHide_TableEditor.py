8c74f41f36483a8eb5617ca9b6fcf58ae0063093
ijgnd <fooijgnd@barijgnd>
2020-07-02 19:14:49 +0200

    use toolbar.py, misc cosmetic, new-style-hooks

--- src/browser_MaximizeHide_TableEditor.py
+++ src/browser_MaximizeHide_TableEditor.py
@@ -5,7 +5,7 @@ For the latter there are two states "editor only" and "sidebar with table maximi
 
 
 """
-copyright 2019 ijgnd
+copyright 2019- ijgnd
           2015-2018 Glutanimate  <https://glutanimate.com/>
           2018 Arthur Milchior
 
@@ -29,20 +29,31 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from aqt.browser import Browser
 from anki.hooks import addHook, wrap
 from anki.lang import _
-from aqt.qt import *
-from aqt import mw
+from aqt.gui_hooks import (
+    browser_menus_did_init,
 
+)
+from aqt import mw
+from aqt.qt import (
+    QKeySequence,
+)
+
+from .toolbar import getMenu
 
 
 def gc(arg, fail=False):
-    return mw.addonManager.getConfig(__name__).get(arg, fail)
+    conf = mw.addonManager.getConfig(__name__)
+    if conf:
+        return conf.get(arg, fail)
+    else:
+        return fail
 
 
 def mysetupTable(self):
-    self.form.fieldsArea.setMinimumSize(50,1)
-    self.form.widget.setMinimumSize(50,1)
+    self.form.fieldsArea.setMinimumSize(50, 1)
+    self.form.widget.setMinimumSize(50, 1)
     self.extremestate = 0   # for toggling views
-    self.advbrowse_uniqueNote_state_original = mw.col.conf.get("advbrowse_uniqueNote", False)
+    self.advbrowse_uniqueNote_state_original = mw.col.get_config("advbrowse_uniqueNote", False)
 
     val = gc("splitter_bigger", False)
     if val:
@@ -50,15 +61,14 @@ def mysetupTable(self):
         if bool(val):
             val = 20
         self.form.splitter.setHandleWidth(val)
-Browser.setupTable = wrap(Browser.setupTable,mysetupTable,"before")
+Browser.setupTable = wrap(Browser.setupTable, mysetupTable, "before")
 
 
 def my_toggle_notes_only(self, arg):
     self.model.beginReset()
-    mw.col.conf["advbrowse_uniqueNote"] =  arg
+    mw.col.set_config("advbrowse_uniqueNote", arg)
     self.onSearchActivated()
     self.model.endReset()
-Browser.my_toggle_notes_only = my_toggle_notes_only
 
 
 def get_splitter_dimension(self):
@@ -72,30 +82,28 @@ def get_splitter_dimension(self):
     except:
         sh = self.form.splitter.size().height()
     return sh
-Browser.get_splitter_dimension = get_splitter_dimension
 
 
 def editor_only(self):
     #note only
-    self.advbrowse_uniqueNote_state_original = mw.col.conf.get("advbrowse_uniqueNote", False)
-    self.my_toggle_notes_only(True)
+    self.advbrowse_uniqueNote_state_original = mw.col.get_config("advbrowse_uniqueNote", False)
+    my_toggle_notes_only(self, True)
     if self.sidebarDockWidget.isVisible():
         self.sidebarDockWidget.setVisible(False)
-    sh = self.get_splitter_dimension()
+    sh = get_splitter_dimension(self)
     self.form.splitter.setSizes([ sh * 0.1, sh * 0.99])  #https://stackoverflow.com/a/47843697
     self.extremestate = 1
-Browser.editor_only = editor_only
 
 
 def table_only(self):
-    self.my_toggle_notes_only(self.advbrowse_uniqueNote_state_original)
+    my_toggle_notes_only(self, self.advbrowse_uniqueNote_state_original)
     # don't hide the editor via  self.form.fieldsArea.setVisible(False) so
     # that I can restore by dragging the splitter
     # ---
     # it's not enough to hide self.tableView because tableView and the search bar
     # are in a widget. If I hide the tableView this widget will only hold the
     # search bar and a lot of grey space.
-    sh = self.get_splitter_dimension()
+    sh = get_splitter_dimension(self)
     self.form.splitter.setSizes([ sh * 0.99, sh * 0.01])  #https://stackoverflow.com/a/47843697
     self.extremestate = 0
     if self.sidebarDockWidget.isVisible():
@@ -114,32 +122,28 @@ def table_only(self):
     #doesn't work
     #sw = self.form.splitter.size().width()
     #self.form.widget.resize(sw,sh)
-Browser.table_only = table_only
 
 
 def toggle_extremes(self):
     if not self.form.fieldsArea.isVisible():
-        self.table_only()
+        table_only(self)
     elif self.extremestate == 0:
-        self.editor_only()
+        editor_only(self)
     else:
-        self.table_only()
-Browser.toggle_extremes = toggle_extremes
+        table_only(self)
 
 
 def back_to_default(self):
-    self.my_toggle_notes_only(self.advbrowse_uniqueNote_state_original)
-    mw.col.conf["advbrowse_uniqueNote"] =  self.advbrowse_uniqueNote_state_original
-
+    my_toggle_notes_only(self, self.advbrowse_uniqueNote_state_original)
+    mw.col.set_config("advbrowse_uniqueNote", self.advbrowse_uniqueNote_state_original)
     if not self.form.widget.isVisible():
         self.form.widget.setVisible(True)
     if not self.form.fieldsArea.isVisible():
         self.form.fieldsArea.setVisible(True)
     if not self.sidebarDockWidget.isVisible():
         self.sidebarDockWidget.setVisible(True)
-    sh = self.get_splitter_dimension()
+    sh = get_splitter_dimension(self)
     self.form.splitter.setSizes([ sh * 0.5, sh * 0.5])  #https://stackoverflow.com/a/47843697
-Browser.back_to_default = back_to_default
 
 
 def my_toggle_sidebar(self):
@@ -147,38 +151,32 @@ def my_toggle_sidebar(self):
         self.sidebarDockWidget.setVisible(True)
     else:
         self.sidebarDockWidget.setVisible(False)
-Browser.my_toggle_sidebar = my_toggle_sidebar
 
 
 def onSetupMenus(self):
-    try:
-        m = self.menuView
-    except:
-        self.menuView = QMenu("&View")
-        action = self.menuBar().insertMenu(
-            self.mw.form.menuTools.menuAction(), self.menuView)
-        m = self.menuView
+    # self is browser
+    m = getMenu(self, "&View")
 
     #a.setCheckable(True)/a.toggled.connect(self.toggleTableAndSidebar) and maybe a.setChecked(True)
     #doesn't offer benefits here: you don't need visual feedbar in the menu
     #to see if the table view is hidden ...
     a = m.addAction('editor only')
-    a.triggered.connect(self.editor_only)
+    a.triggered.connect(lambda _, browser=self: editor_only(browser))
     a.setShortcut(QKeySequence(gc("hotkey_editor_only","")))
 
     a = m.addAction('table only')
-    a.triggered.connect(self.table_only)
+    a.triggered.connect(lambda _, browser=self: table_only(browser))
     a.setShortcut(QKeySequence(gc("hotkey_table_only","")))
 
     a = m.addAction('toggle between "only table/sidebar" and "only editor"')
-    a.triggered.connect(self.toggle_extremes)
+    a.triggered.connect(lambda _, browser=self: toggle_extremes(browser))
     a.setShortcut(QKeySequence(gc("hotkey_toggle","")))
 
     a = m.addAction('toggle sidebar')
-    a.triggered.connect(self.my_toggle_sidebar)
+    a.triggered.connect(lambda _, browser=self: my_toggle_sidebar(browser))
     a.setShortcut(QKeySequence(gc("hotkey_toggle_sidebar","")))
 
     a = m.addAction('reset to default')
-    a.triggered.connect(self.back_to_default)
+    a.triggered.connect(lambda _, browser=self: back_to_default(browser))
     a.setShortcut(QKeySequence(gc("hotkey_back_to_default","")))
-addHook("browser.setupMenus", onSetupMenus)
+browser_menus_did_init.append(onSetupMenus)
